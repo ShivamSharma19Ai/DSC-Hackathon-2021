35d1c76dabc81b0eaf403579924e7a3d8a462385
SHIVAM-AIML <shivam.sharma19sept@gmail.com>
2021-05-02 16:27:06 +0530

    added contact.html

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, session 
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
@@ -23,11 +25,6 @@ app.config['MYSQL_DB'] = 'flaskapp'
 mysql = MySQL(app)
 
 
-
-
-
-
-        
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
@@ -39,7 +36,8 @@ def login():
         password = request.form['password']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
         # If account exists in accounts table in out database
@@ -56,37 +54,42 @@ def login():
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
     # http://localhost:5000/python/logout - this will be the logout page
+
+
 @app.route('/pythonlogin/logout')
 def logout():
     # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
 
-   # Check if account exists using MySQL
-   cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-   cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-   account = cursor.fetchone()
-   # If account exists show error and validation checks
-   if account:
-    msg = 'Account already exists!'
-   elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-    msg = 'Invalid email address!'
-   elif not re.match(r'[A-Za-z0-9]+', username):
-    msg = 'Username must contain only characters and numbers!'
-   elif not username or not password or not email:
-    msg = 'Please fill out the form!'
-   else:
-    # Account doesnt exists and the form data is valid, now insert new account into accounts table
-    cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-    mysql.connection.commit()
-    msg = 'You have successfully registered!'
+    # Check if account exists using MySQL
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+    account = cursor.fetchone()
+    # If account exists show error and validation checks
+    if account:
+        msg = 'Account already exists!'
+    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        msg = 'Invalid email address!'
+    elif not re.match(r'[A-Za-z0-9]+', username):
+        msg = 'Username must contain only characters and numbers!'
+    elif not username or not password or not email:
+        msg = 'Please fill out the form!'
+    else:
+        # Account doesnt exists and the form data is valid, now insert new account into accounts table
+        cursor.execute(
+            'INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered!'
+
 
 @app.route("/see")
 def index():
@@ -115,7 +118,8 @@ def upload():
         if (ext == ".jpg") or (ext == ".png"):
             print("File supported moving on...")
         else:
-            render_template("Error.html", message="Files uploaded are not supported...")
+            render_template(
+                "Error.html", message="Files uploaded are not supported...")
         destination = "/".join([target, filename])
         print("Accept incoming file:", filename)
         print("Save it to:", destination)
@@ -135,6 +139,12 @@ def get_gallery():
     image_names = os.listdir('./images')
     print(image_names)
     return render_template("gallery.html", image_names=image_names)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 
 
